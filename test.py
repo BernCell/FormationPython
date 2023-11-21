92385982eb7603de310704a6ca606e6161a18efe
BernCell <bern.cell@hotmail.com>
2023-11-21 23:36:32 +0100

    Les Boucles : for avec in et range()

--- test.py
+++ test.py
@@ -871,6 +871,22 @@ elif mdp.isdigit():
 else:
     print("Inscription Terminée.")
     
+""" Les Boucles """
+# La Boucle for 
+## Répète une opération un certain nombre de fois
+## Parcourt des structures de données
+
+#for element in liste:
+    #print(element)
+
+for i in  [0, 1, 2, 3, 4, 7, 9]:
+    print(i)
+    
+for lettre in "Python":
+    print(lettre)
+    
+for i in range(4):
+    print("Bring the noise")
 
 
 
